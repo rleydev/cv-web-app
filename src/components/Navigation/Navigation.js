a45882c151b3265ce47ebe9256027da26a9bcd3e
Arthur Lee <rley.dev@gmail.com>
2023-05-12 20:30:00 +0600

    feat: animatiion side

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,41 +4,41 @@ import { faUser, faGraduationCap, faPencil, faSuitcase, faLocationArrow, faComme
 import './Navigation.scss'
 import { Link } from 'react-scroll'
 
-const Navigation = () => {
+const Navigation = ({panelState}) => {
 
     return (
         <article className='navigation'>
-            <Link to='about' className='navigation__container' spy={true} smooth={true} duration={500} activeClass='current-page-location'>
+            <Link to='about' className={panelState === false ? 'navigation__container' : 'nav--hidden'} spy={true} smooth={true} duration={500} activeClass='current-page-location'>
                 <FontAwesomeIcon className='navigation__container__icon' icon={faUser} color='#667081' />
                 <p className='navigation__container__link'>
                     About me
                 </p>
             </Link>
-            <Link to='education' className='navigation__container' spy={true} smooth={true} duration={500} activeClass='current-page-location'>
+            <Link to='education' className={panelState === false ? 'navigation__container' : 'nav--hidden'} spy={true} smooth={true} duration={500} activeClass='current-page-location'>
                 <FontAwesomeIcon className='navigation__container__icon' icon={faGraduationCap} color='#667081' />
                 <p className='navigation__container__link'>
                     Education
                 </p>
             </Link>
-            <Link to='experience' className='navigation__container' spy={true} smooth={true} duration={500} activeClass='current-page-location'>
+            <Link to='experience' className={panelState === false ? 'navigation__container' : 'nav--hidden'} spy={true} smooth={true} duration={500} activeClass='current-page-location'>
                 <FontAwesomeIcon className='navigation__container__icon' icon={faPencil} color='#667081' />
                 <p className='navigation__container__link'>
                     Experience
                 </p>
             </Link>
-            <Link to='portfolio' className='navigation__container' spy={true} smooth={true} duration={500} activeClass='current-page-location'>
+            <Link to='portfolio' className={panelState === false ? 'navigation__container' : 'nav--hidden'} spy={true} smooth={true} duration={500} activeClass='current-page-location'>
                 <FontAwesomeIcon className='navigation__container__icon' icon={faSuitcase} color='#667081' />
                 <p className='navigation__container__link'>
                     Portfolio
                 </p>
             </Link>
-            <Link to='contacts' className='navigation__container' spy={true} smooth={true} duration={500} activeClass='current-page-location'>
+            <Link to='contacts' className={panelState === false ? 'navigation__container' : 'nav--hidden'} spy={true} smooth={true} duration={500} activeClass='current-page-location'>
                 <FontAwesomeIcon className='navigation__container__icon' icon={faLocationArrow} color='#667081' />
                 <p className='navigation__container__link'>
                     Contacts
                 </p>
             </Link>
-            <Link to='feedback' className='navigation__container' spy={true} smooth={true} duration={500} activeClass='current-page-location'>
+            <Link to='feedback' className={panelState === false ? 'navigation__container' : 'nav--hidden'} spy={true} smooth={true} duration={500} activeClass='current-page-location'>
                 <FontAwesomeIcon className='navigation__container__icon' icon={faComment} color='#667081' />
                 <p className='navigation__container__link'>
                     Feedback
--- src/components/Panel/Panel.js
+++ src/components/Panel/Panel.js
@@ -5,24 +5,38 @@ import Navigation from '../Navigation/Navigation'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons'
+import { useEffect, useState } from 'react'
 
  
 const Panel = ({data}) => {
 
+    const [togglePanel, setTogglePanel] = useState(false)
+
+    const panelStickPressed = () => {
+        if (togglePanel === false) {
+            setTogglePanel(true)
+        } else if (togglePanel === true) {
+            setTogglePanel(false)
+        }
+    }
+
+    useEffect(() => {
+        console.log(togglePanel)
+    }, [togglePanel])
 
      return (
-        <article className='panel'>
-            <div className='panel__stick'>
+        <article className={togglePanel === false ? `panel` : 'panel--hiden'}>
+            <div className='panel__stick' onClick={panelStickPressed}>
                     <img className='panel__stick__img' src={stick} alt='stick' />
             </div>
-            <div className='panel__photo-container'>
+            <div className={togglePanel === false ? 'panel__photo-container' : 'panel-photo--hiden'}>
                 <img className='panel__photo-container__photo' src={avatar} alt='avatar' />
                 <h2 className='panel__photo-container__name'>{data}</h2>
             </div>
-            <div className='panel__nav-container'>
-                <Navigation />
+            <div className={togglePanel === false ? 'panel__nav-container' : 'panel--hiden'}>
+                <Navigation panelState={togglePanel} />
             </div>
-            <Link to='/' className='panel__button'>
+            <Link to='/' className={togglePanel === false ? 'panel__button' : 'panel-button--hiden'}>
                 <button className="panel__button__but">
                     <FontAwesomeIcon icon={faAngleLeft} />
                     <p className='panel__button__but__text'>Go back</p>
